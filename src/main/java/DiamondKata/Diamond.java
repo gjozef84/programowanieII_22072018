5c8366a3be1f03c992ae34d9dad42f9e790df8ad
Grzesiek Józefowicz <gjozef84@gmail.com>
2018-07-22 21:31:29 +0200

    Return Diamond work :-)

---
+++ src/main/java/DiamondKata/Diamond.java
@@ -0,0 +1,75 @@
+package DiamondKata;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Collections.reverse;
+import static org.apache.commons.lang3.StringUtils.repeat;
+import static org.apache.commons.lang3.StringUtils.startsWith;
+
+/**
+ * Created by Grzesiek on 2018-07-22
+ */
+public class Diamond {
+    public static final char START_LETTER = 'A';
+
+    public String diamondGenerator(char letter) {
+
+        if (letter == 65) return String.valueOf(letter);
+        else {
+
+            String diamond = "";
+
+            List<String> stringList = createRows(letter);
+            List<String> rewersList = reversRows(stringList);
+            stringList.addAll(rewersList);
+
+            for (int i = 0; i < stringList.size(); i++) {
+                if (i < stringList.size() - 1) diamond += stringList.get(i) + "\n";
+                else diamond += stringList.get(i);
+            }
+
+            return diamond;
+        }
+    }
+
+    public static List<String> createRows(char letter) {
+
+        List<String> rows = new ArrayList<>();
+        int letterInterval = letter - START_LETTER;
+        for (int letterIndex = 0; letterIndex <= letterInterval; letterIndex++) {
+            rows.add(rowBuilder(letter, letterIndex));
+        }
+        return rows;
+    }
+
+    public static String rowBuilder(char letter, int whichRow) {
+        String row = "";
+        char aktualnaLetter = (char) (START_LETTER + whichRow);
+        String outherDashes = dashes(letter - aktualnaLetter);
+
+        row += outherDashes + aktualnaLetter + innerDashes(aktualnaLetter);
+        if (whichRow > 0) row += aktualnaLetter;
+        row += outherDashes;
+
+        return row;
+    }
+
+    public static List<String> reversRows(List<String> toRevers) {
+        List<String> reversRows = new ArrayList<>(toRevers);
+        reverse(reversRows);
+        reversRows.remove(0);
+        return reversRows;
+    }
+
+    private static String innerDashes(int aktualnaLetter) {
+        int lettersInterval = aktualnaLetter - START_LETTER;
+        return dashes((lettersInterval * 2) - 1);
+    }
+
+    public static String dashes(int number) {
+        return repeat(' ', number);
+    }
+}
+
---
+++ src/test/java/DiamondKata/DiamondTest.java
@@ -0,0 +1,43 @@
+package DiamondKata;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Grzesiek on 2018-07-22
+ */
+public class DiamondTest {
+
+    Diamond diamond;
+
+    @Before
+    public void setUp() throws Exception {
+        diamond = new Diamond();
+    }
+
+    @Test
+    public void diamondGeneratorOnlyA() {
+        String expDiamond="A";
+        assertEquals(expDiamond, diamond.diamondGenerator('A'));
+    }
+
+    @Test
+    public void diamondGeneratorB() {
+        String expDiamond=" A \n" +
+                          "B B\n" +
+                          " A ";
+        assertEquals(expDiamond, diamond.diamondGenerator('B'));
+    }
+
+    @Test
+    public void diamondGeneratorC() {
+        String expDiamond="  A  \n" +
+                          " B B \n" +
+                          "C   C\n"+
+                          " B B \n" +
+                          "  A  ";
+        assertEquals(expDiamond, diamond.diamondGenerator('C'));
+    }
+}
