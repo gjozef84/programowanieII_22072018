de8dbea7d720d31fa3f7ad302b164f0d798ebb30
Grzesiek Józefowicz <gjozef84@gmail.com>
2018-07-22 10:37:36 +0200

    Add 9 tests

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -4,7 +4,18 @@
 public class FizzBuzz {
 
     public static String PrintRange(int from, int to) {
-        return "1\n2";
+        String stringOut = "";
+        if (!(from<100 && to > 1)) throw new IllegalArgumentException("Support range from 1 to 100");
+        if (from > to) throw new IllegalArgumentException();
+
+        for (int i = from; i <= to; i++) {
+            if (i % 3 == 0 && i % 5 == 0) stringOut += "FizzBuzz!\n";
+            else if (i % 3 == 0 || String.valueOf(i).contains("3")) stringOut += "Fizz\n";
+            else if (i % 5 == 0 || String.valueOf(i).contains("5")) stringOut += "Buzz\n";
+            else stringOut += i + "\n";
+        }
+
+        return stringOut;
     }
 }
 
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -9,12 +9,50 @@ public class FizzBuzzTest {
 
     @Test
     public void show_numbers1and2(){
-        assertEquals("1\n2", FizzBuzz.PrintRange(1,2));
+        assertEquals("1\n2\n", FizzBuzz.PrintRange(1,2));
     }
 
     @Test
     public void showNumbersFrom_To(){
-        assertEquals("1\n2\n3\n4\n5\n6", FizzBuzz.PrintRange(1,6));
+        assertEquals("1\n2\nFizz\n4\nBuzz\nFizz\n", FizzBuzz.PrintRange(1,6));
     }
 
+    @Test
+    public void showFizz(){
+        assertEquals("1\n2\nFizz\n", FizzBuzz.PrintRange(1,3));
+    }
+
+    @Test
+    public void showFizzFrom_1_To_9(){
+        assertEquals("1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\n", FizzBuzz.PrintRange(1,9));
+    }
+
+    @Test
+    public void showBuzz(){
+        assertEquals("1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n", FizzBuzz.PrintRange(1,10));
+    }
+
+    @Test
+    public void showFizzBuzz(){
+        assertEquals("1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\nFizz\n14\nFizzBuzz!\n", FizzBuzz.PrintRange(1,15));
+    }
+    @Test
+    public void showFizzWhenNumberContains3(){
+        assertEquals("Fizz\nFizz\n14\nFizzBuzz!\n", FizzBuzz.PrintRange(12,15));
+    }
+
+    @Test
+    public void showBuzzWhenNumberContains5(){
+        assertEquals("Buzz\nFizz\n22\nFizz\nFizz\nBuzz\n", FizzBuzz.PrintRange(20,25));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void supportRangeFrom1to100(){
+        FizzBuzz.PrintRange(101,103);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFromLessTo(){
+        FizzBuzz.PrintRange(105,103);
+    }
 }
