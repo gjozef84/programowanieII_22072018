519f19a9bd47552480f23bfb2c9ee4abec345b44
Grzesiek Józefowicz <gjozef84@gmail.com>
2018-07-26 21:35:01 +0200

    Refactor

--- src/test/java/FizzBuzz/FizzBuzzTest.java
+++ src/test/java/FizzBuzz/FizzBuzzTest.java
@@ -1,3 +1,6 @@
+package FizzBuzz;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -9,41 +12,41 @@ public class FizzBuzzTest {
 
     @Test
     public void show_numbers1and2(){
-        assertEquals("1\n2\n", FizzBuzz.PrintRange(1,2));
+        Assert.assertEquals("1\n2\n", FizzBuzz.PrintRange(1,2));
     }
 
     @Test
     public void showNumbersFrom_To(){
-        assertEquals("1\n2\nFizz\n4\nBuzz\nFizz\n", FizzBuzz.PrintRange(1,6));
+        Assert.assertEquals("1\n2\nFizz\n4\nBuzz\nFizz\n", FizzBuzz.PrintRange(1,6));
     }
 
     @Test
     public void showFizz(){
-        assertEquals("1\n2\nFizz\n", FizzBuzz.PrintRange(1,3));
+        Assert.assertEquals("1\n2\nFizz\n", FizzBuzz.PrintRange(1,3));
     }
 
     @Test
     public void showFizzFrom_1_To_9(){
-        assertEquals("1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\n", FizzBuzz.PrintRange(1,9));
+        Assert.assertEquals("1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\n", FizzBuzz.PrintRange(1,9));
     }
 
     @Test
     public void showBuzz_WhenNumbersDivideBy_5(){
-        assertEquals("1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n", FizzBuzz.PrintRange(1,10));
+        Assert.assertEquals("1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n", FizzBuzz.PrintRange(1,10));
     }
 
     @Test
     public void showFizzBuzz_WhenNumberDivideBy_3_and_By_5(){
-        assertEquals("1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\nFizz\n14\nFizzBuzz!\n", FizzBuzz.PrintRange(1,15));
+        Assert.assertEquals("1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\nFizz\n14\nFizzBuzz!\n", FizzBuzz.PrintRange(1,15));
     }
     @Test
     public void showFizz_WhenNumberContains_3(){
-        assertEquals("Fizz\nFizz\n14\nFizzBuzz!\n", FizzBuzz.PrintRange(12,15));
+        Assert.assertEquals("Fizz\nFizz\n14\nFizzBuzz!\n", FizzBuzz.PrintRange(12,15));
     }
 
     @Test
     public void showBuzz_WhenNumberContains_5(){
-        assertEquals("Buzz\nFizz\n22\nFizz\nFizz\nBuzz\n", FizzBuzz.PrintRange(20,25));
+        Assert.assertEquals("Buzz\nFizz\n22\nFizz\nFizz\nBuzz\n", FizzBuzz.PrintRange(20,25));
     }
 
     @Test(expected = IllegalArgumentException.class)
